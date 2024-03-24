b1660ee9ac8a94cb859ef4ba87173e89354ef319
Irina Kriuchkova <ivk2005@yandex.ru>
2024-03-24 19:17:21 +0300

    Задание выполнено частично (неправильный счетчик удачных попаданий)

---
+++ src/js/GameController.js
@@ -0,0 +1,52 @@
+export default class GameController {
+    constructor(gamePlay) {
+        this.gamePlay = gamePlay;
+    } 
+
+    init() {
+        this.gamePlay.drawUi();
+
+        let onClick = false;
+        let position;
+
+        this.countSuccess = 0;                    // счетчик удачных попаданий   
+        this.fail = 0;                            // счетчик промахов
+
+        // this.onCellClick = this.onCellClick.bind(this);
+        let countSuccessEl = document.querySelector(".status-wins");                         
+        let countfailEl = document.querySelector(".status-lost");               
+
+        document.addEventListener('click', (event) => {
+            onClick = this.onCellClick(event);
+        });
+        // document.addEventListener('click', () => {onClick = true});
+
+        const redrawInterval = setInterval(() => {
+            if (!onClick) {
+                this.fail++;
+            }
+            onClick = false;
+            countSuccessEl.textContent = this.countSuccess;
+            countfailEl.textContent = this.fail;
+            
+            if (this.fail === 6) {
+                clearInterval(redrawInterval);
+                alert("Вы проиграли");
+                this.init();
+            }
+            console.log(this.countSuccess, this.fail);
+
+            position = Math.floor(Math.random() * (this.gamePlay.boardSize) ** 2);
+            this.gamePlay.redrawPosition(position);
+        }, 1000);
+    }
+
+    onCellClick(e) {
+        const target = e.target;
+        
+        if(target.classList.contains('character')) {
+            this.countSuccess++;
+            return true;
+        }
+    }
+}
